920a1764237fbcc3d298e6d1c58803ea2bf96c85
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-09-27 00:32:37 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,51 +1,25 @@
-# Define the class for Nginx installation and configuration
-class nginx_config {
-  package { 'nginx':
-    ensure => 'installed',
-  }
+#  Install and configure an Nginx server using Puppet
 
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  # Create an Nginx configuration file
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx_config/nginx.conf.erb'),
-    notify  => Service['nginx'],
-  }
-
-  # Ensure Nginx is listening on port 80
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    require => Package['nginx'],
-  }
-
-  # Create the Hello World HTML file
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  # Create a symbolic link to enable the site
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => link,
-    target  => '/etc/nginx/sites-available/default',
-    require => File['/etc/nginx/sites-available/default'],
-  }
-
-  # Define the 301 redirect location
-  nginx::resource::location { 'redirect_me':
-    ensure       => present,
-    location     => '/redirect_me',
-    return       => '301',
-    return_value => 'https://www.example.com/new_location',
-    require      => Package['nginx'],
-  }
+package {'nginx':
+  ensure => 'present',
 }
 
-# Apply the Nginx configuration class
-include nginx_config
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
