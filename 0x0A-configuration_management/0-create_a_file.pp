5af3be691d71cc3b16b3330de4f28992c9076a36
iyiolakeni <iyiolakeni@gmail.com>
2023-09-29 12:09:26 -0700

    configuration assignment

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a /tmp in the file
+
+file { 'school' :
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    path    => '/tmp/school'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#  Kills a process named killmenow
+
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
+}
