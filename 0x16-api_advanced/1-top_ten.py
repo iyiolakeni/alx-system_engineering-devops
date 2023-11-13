12f84c1c4ffe8a944aede2de328164efd6e4eb74
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-11-13 23:35:14 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Function to query the Reddit API."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-agent": "Google Chrome Version 119.0.6045.123"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
-        return 0
+        print("None")
+        return
     results = response.json().get("data")
-    return results.get("subscribers")
+    [print(c.get("data").get("title")) for c in results.get("children")]
